35fe35cb2483e63d015612b0b543701af4c69248
Azar Mamiyev <heyitsazar@gmail.com>
2024-03-14 15:31:18 +0100

    Added decryption

--- encryption_script.py
+++ encryption_script.py
@@ -44,23 +44,35 @@ if checkKey() == True:
     # transposition (encryption)
     # creating a list with 0 values in order to change them later according to our key
     secret_message = ['00000000' for binary_element in binary_message]
+    decrypted_message = ['00000000' for binary_element in binary_message]
     
     # this loop is for changing every character of our secret_message list, but since [] is immutable we're making it mutable inside this loop
     for i, binary_letter in enumerate(binary_message):
-        temp_message[i] = list(temp_message[i])
+        secret_message[i] = list(secret_message[i])
         for j, binary_value in enumerate(binary_letter):
-            temp_message[i][int(key[j])-1]=binary_message[i][j]
-    
-    # and here we're making sure that the values that we converted to be mutable stick together
-    temp_message = [''.join(temp_message[i]) for i in range(len(temp_message))]
-    
-    for i, binary_letter in enumerate(binary_message):
-        temp_message[i] = list(temp_message[i])
-        for j, binary_value in enumerate(binary_letter):
-            temp_message[i][int(key[j])-1]=binary_message[i][j]
+            secret_message[i][int(key[j])-1]=binary_message[i][j]
 
+    # decryption
+    # generating the decryption key
+    decryption_key = [key.index(str(i)) + 1 for i in range(1, len(key) + 1)]
+
+    # decrypting the message with the same way that we encrypt
+    print(decryption_key)
+    for i, binary_letter in enumerate(secret_message):
+        decrypted_message[i] = list(decrypted_message[i])
+        for j, binary_value in enumerate(binary_letter):
+            decrypted_message[i][int(decryption_key[j])-1]=secret_message[i][j]
+
+    # and here we're making sure that the values that we converted to be mutable stick together
+    secret_message = [''.join(secret_message[i]) for i in range(len(secret_message))]
+    decrypted_message = [''.join(decrypted_message[i]) for i in range(len(decrypted_message))]
 
     ascii = [chr(int(binary, 2)) for binary in binary_message]
-    secret_ascii = [chr(int(binary, 2)) for binary in temp_message]
+    secret_ascii = [chr(int(binary, 2)) for binary in secret_message]
+    decrypted_ascii = [chr(int(binary, 2)) for binary in decrypted_message]
+
+    print("Original key: " + str(key))
+    print("Decryption key: " + str(decryption_key))
     print("Original message: " + str(ascii))
     print("Encrypted message: " + str(secret_ascii))
+    print("Decrypted message: " + str(decrypted_ascii))
